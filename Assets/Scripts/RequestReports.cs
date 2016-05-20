8178f554a40c824bf6c47068956b72255b8af6ce
Athanasios <a.vourvopoulos@gmail.com>
2016-05-20 20:37:11 +0300

    first commit

---
+++ Assets/Scripts/RequestReports.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class RequestReports : MonoBehaviour {
+
+	
+	
+	void Start()
+	{
+		StartCoroutine(SendRequest());
+	}
+	
+	
+	IEnumerator SendRequest()
+	{
+		WWW request = new WWW("http://derecho.inf.usi.ch/WeatherUSI/getPlacesWithReports.php");
+		
+		yield return request;
+		
+		if (request.error == null || request.error == "")
+		{
+			var N = JSON.Parse(request.text);
+			int placesLength = N["places"].Count;
+			for (int i=0; i<=placesLength; i++)
+			{
+				if(N["places"][i]["place_name"].Value.ToString() != "unknown")
+					Debug.Log(N["places"][i]["place_name"].Value.ToString());
+			}
+			
+		}
+		else
+		{
+			Debug.Log("WWW error: " + request.error);
+		}
+		
+	}
+	
+
+}
